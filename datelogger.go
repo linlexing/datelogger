40b7ed8efede865a8ccb9aa04594093f62d61638
Bo0 <425022819@qq.com>
2024-11-12 08:58:02 +0800

    删除自带的log，只负责写文件夹下的分日期文件

--- datelogger.go
+++ datelogger.go
@@ -1,49 +1,30 @@
 package datelogger
 
 import (
-	"io"
 	"os"
 	"path/filepath"
 	"time"
-
-	"github.com/sirupsen/logrus"
 )
 
-//DateLogger 自动按照日期分文件写入日志
+// DateLogger 自动按照日期分文件写入日志
 type DateLogger struct {
 	path        string //存放的目录名
 	logFile     *os.File
 	logFileName string //当前的日志文件名
-	log         *logrus.Logger
 }
 
-//NewDateLogger 分配一个按日期存放 文件的日志
+// NewDateLogger 分配一个按日期存放 文件的日志
 func NewDateLogger(strPath string) *DateLogger {
 	d := &DateLogger{
 		path: strPath,
 	}
-	l := &logrus.Logger{
-		Formatter: &logrus.TextFormatter{
-			TimestampFormat: "20060102T150405",
-		},
-		Hooks: make(logrus.LevelHooks),
-		Out:   io.MultiWriter(d, os.Stdout),
-		Level: logrus.DebugLevel,
-	}
-	d.log = l
 	return d
 }
 
-//Log 返回实际的logrus
-func (d *DateLogger) Log() *logrus.Logger {
-	return d.log
-}
-
-//Close 关闭底层文件
+// Close 关闭底层文件
 func (d *DateLogger) Close() error {
 	if d.logFile != nil {
 		err := d.logFile.Close()
-		d.log = nil
 		d.logFile = nil
 		return err
 	} else {
@@ -51,7 +32,7 @@ func (d *DateLogger) Close() error {
 	}
 }
 
-//Write 被logrus 调用来写日志，自动分文件
+// Write 被logrus 调用来写日志，自动分文件
 func (d *DateLogger) Write(p []byte) (n int, err error) {
 	if err := d.checkLogFile(); err != nil {
 		return -1, err
